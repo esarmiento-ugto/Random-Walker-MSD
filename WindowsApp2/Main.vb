b3664a8d16d688b2703f4e621563c0d55d7d49c8
esarmiento-ugto <100631394+esarmiento-ugto@users.noreply.github.com>
2022-03-07 10:43:01 -0600

    Update Main.vb

--- WindowsApp2/Main.vb
+++ WindowsApp2/Main.vb
@@ -61,7 +61,7 @@ Public Class Main
         Chart1.ChartAreas(0).AxisX.IsLogarithmic = True
         Chart1.ChartAreas(0).AxisY.IsLogarithmic = True
         For j = 0 To Nmax - 1
-            writer1.WriteLine(x(j))
+            writer1.WriteLine(j * TextBox4.Text & Chr(9) & x(j))
             Chart2.Series("tray").Points.AddXY(j * TextBox4.Text, x(j))
         Next
         writer1.Close()
